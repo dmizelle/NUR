92137214a23f531a93fdace2f08cd27f370fcc50
Devon Mizelle <devon.mizelle@onepeloton.com>
2022-06-01 15:56:38 -0400

    adding kodi-moonlight-qt

--- default.nix
+++ default.nix
@@ -9,12 +9,10 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  # The `lib`, `modules`, and `overlay` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  kodi-moonlight-qt =  pkgs.callPackage ./pkgs/kodi-moonlight-qt {};
 }
--- flake.nix
+++ flake.nix
@@ -1,21 +1,10 @@
 {
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: rec {
+      packages =
+        import ./default.nix { pkgs = import nixpkgs { inherit system; }; };
+    });
 }
